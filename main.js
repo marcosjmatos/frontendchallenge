e0e0964932af07dfde1374ba8c76985951da8274
Marcos <marcosjmatosbrache@gmail.com>
2022-05-24 21:01:03 -0400

    challenge frontend

---
+++ main.js
@@ -0,0 +1,19 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "150px";
+    document.getElementById("menu").style.marginRight = "150px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("menu").style.marginRight= "0";
+  }
+
+
+
+
+
+
+
+
+
+
