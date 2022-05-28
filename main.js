1984dcc82c95d39f06e02bf4777da9dbedc6c4e2
Marcos <marcosjmatosbrache@gmail.com>
2022-05-27 23:16:35 -0400

    Co-authored-by: geniusss16 <geniusss16@users.noreply.github.com> Co-authored-by: lematosdev <lematosdev@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
+const desplegable = document.querySelector(".features");
+const desplegable2 = document.querySelector('.company');
+const contenido = document.querySelector(".content")
+const contenido2 = document.querySelector(".content2")
+const flecha1 = document.querySelector(".flecha1");
+const flecha2 = document.querySelector(".flecha2");
+
+
 function openNav() {
-    document.getElementById("mySidebar").style.width = "150px";
-    document.getElementById("menu").style.marginRight = "150px";
+  document.getElementById("mySidebar").style.width = "150px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
+
+
+desplegable.addEventListener("click", ()=> {
+  menu1(contenido,flecha1);
+}) 
+
+//dropdown the menu funtion
+function menu1(element,flecha) {
+
+  if (element.style.display === "inline-block") {
+    element.style.display = "none";
+    flecha.setAttribute("src","images/icon-arrow-down.svg")
+  } else {
+    element.style.display = "inline-block"
+    flecha.setAttribute("src","images/icon-arrow-up.svg")
+
   }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("menu").style.marginRight= "0";
-  }
+
+}
+
+desplegable2.addEventListener("click", ()=> {
+
+menu1(contenido2,flecha2);
+
+}) 
 
 
 
+// target    .setAttribute 'src' direcccion de la imagen 
 
-
-
-
-
-
-
+// fototipo1.setAttribute("src", 'imagenes/.webp');
